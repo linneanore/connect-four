e6d46e4c69806118cde6094bd1185eb71b6909c5
Linnea Nore <nore.linnea@hotmail.com>
2024-09-15 20:04:44 +0200

    method to print state of the board

--- typescript/classes/board.ts
+++ typescript/classes/board.ts
@@ -7,4 +7,14 @@ export class board {
         this.grid = Array.from({ length: this.rows }, () =>
         Array(this.columns).fill(0));
     }
+
+    //printing the current state of the board
+    public printBoard(): void {
+        console.clear();
+        console.log("Current Board:");
+        for (let row of this.grid.reverse()) {
+            console.log(row.map(cell => (cell === 0 ? '.' : cell)).join(' '));
+        }
+        console.log('0 1 2 3 4 5 6');
+    }
 }

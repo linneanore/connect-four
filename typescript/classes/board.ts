07ca97806ed026495eb28707d341e029ed339dc3
Linnea Nore <nore.linnea@hotmail.com>
2024-09-15 20:07:56 +0200

    Drop Piece method

--- typescript/classes/board.ts
+++ typescript/classes/board.ts
@@ -17,4 +17,18 @@ export class board {
         }
         console.log('0 1 2 3 4 5 6');
     }
+
+    //Method to drop piece into column
+    public dropPiece(column: number, player: number): boolean {
+        for (let row = 0; row < this.rows; row++) {
+            //Finding the first empty spot
+            if (this.grid[row] [column] === 0) {
+                //Places the players piece
+                this.grid[row][column] = player;
+                return true;
+            }
+        }
+        //Cannot drop piece if column is full
+        return false;
+    }
 }

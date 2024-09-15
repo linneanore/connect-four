2a768cd3bfd0095d4f30a7c050d63b6a27e1c21a
Linnea Nore <nore.linnea@hotmail.com>
2024-09-15 20:41:15 +0200

    Method for random move AI

--- typescript/classes/ai.ts
+++ typescript/classes/ai.ts
@@ -6,4 +6,19 @@ export class AI {
     constructor(playerId:number) {
         this.playerId = playerId;
     }
+
+    //Method for AI to make move in random column
+    public getMove(board: board): number {
+        const emptyColumns: number[] = [];
+        for (let col = 0; col < 7; col++) {
+            if (board['grid'] [0] [col] === 0) {
+                //If the column is not full
+                emptyColumns.push(col); 
+                // add it to list of empty columns
+            }
+        } 
+        const randomIndex = Math.floor(Math.random() * emptyColumns.length);
+        //Randomly select an empty column
+        return emptyColumns[randomIndex]; 
+    }
 }
